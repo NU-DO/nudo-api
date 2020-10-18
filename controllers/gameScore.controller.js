584a8c3caf96a1ffc90b5c2ff7aecc169c306337
miguelbachi5a <miguelbachi5a@gmail.com>
2020-10-18 13:59:37 +0200

    feat: gameScore and contact controllers

---
+++ controllers/gameScore.controller.js
@@ -0,0 +1,23 @@
+const GameScore = require('../models/gameScore.model')
+const createError = require('http-errors')
+
+module.exports.getScores = (req, res, next) => {
+    GameScore.find({ user: req.session.user.id })
+        .then(gameScores => {
+            console.log('Ver gameScores:', gameScores)
+            res.json(gameScores)
+        })
+        .catch(next)
+}
+
+module.exports.create = (req, res, next) => {
+    const gameScore = new GameScore({
+        user: req.session.user.id,
+        score: req.body.score,
+        level: req.body.level,
+    })
+
+    gameScore.save()
+        .then(gameScore => res.status(201).json(gameScore))
+        .catch(next)
+}
