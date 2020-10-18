f5d974d386521cacc9f78d73beb3b362dc2c6549
miguelbachi5a <miguelbachi5a@gmail.com>
2020-10-18 13:38:17 +0200

    feat: event and appointment cotrollers

---
+++ controllers/event.controller.js
@@ -0,0 +1,41 @@
+const Event = require('../models/event.model')
+const createError = require('http-errors')
+
+module.exports.getEvents = (req, res, next) => {
+    Event.find({ user: req.session.user.id })
+        .then(events => {
+            console.log('Ver events:', events)
+            res.json(events)
+        })
+        .catch(next)
+}
+
+module.exports.create = (req, res, next) => {
+    const event = new Event({
+        user: req.session.user.id,
+        title: req.body.title,
+        year: req.body.year,
+        description: req.body.description,
+        links: req.body.links,
+    })
+
+    event.save()
+        .then(event => res.status(201).json(event))
+        .catch(next)
+}
+
+module.exports.edit = (req, res, next) => {
+    const body = req.body
+
+    Event.findOneAndUpdate({ _id: req.params.id }, body, { runValidators: true, new: true })
+        .then(event => {
+            res.status(201).json({ message: 'edit from event.model' })
+        })
+        .catch(next)
+
+}
+
+module.exports.delete = (req, res, next) => {
+    Event.findByIdAndRemove(req.params.id)
+        .catch(err => console.log(err))
+}
