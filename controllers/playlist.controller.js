04fd775faadee89d771e8fe5bdf245b727eabba5
miguelbachi5a <miguelbachi5a@gmail.com>
2020-10-20 21:14:39 +0200

    feat: add Spotify calls

--- controllers/playlist.controller.js
+++ controllers/playlist.controller.js
@@ -1,5 +1,15 @@
 const Playlist = require('../models/playlist.model')
 const createError = require('http-errors')
+const SpotifyWebApi = require('spotify-web-api-node')
+
+const spotifyApi = new SpotifyWebApi({
+    clientId: process.env.CLIENT_ID,
+    clientSecret: process.env.CLIENT_SECRET
+})
+
+spotifyApi.clientCredentialsGrant()
+    .then(data => spotifyApi.setAccessToken(data.body['access_token']))
+    .catch(error => console.log('Something went wrong when retrieving an access token', error))
 
 module.exports.getSongs = (req, res, next) => {
     Playlist.find({ user: req.session.user.id })
@@ -36,4 +46,21 @@ module.exports.delete = (req, res, next) => {
     Playlist.findByIdAndRemove(req.params.id)
         .then(playlist => res.status(200).json(playlist))
         .catch(err => console.log(err))
+}
+
+module.exports.getArtistsFromSpotify = (req, res, next) => {;
+    spotifyApi.searchArtists(`${req.body.search}`)
+        .then(data => {
+            res.json(data)
+        })
+        .catch(err => console.log('The error while searching artists occurred: ', err))
+}
+
+module.exports.getSongsFromSpotify = (req, res, next) => {;
+    spotifyApi.searchTracks(`${req.body.search}`)
+        .then(data => {
+            console.log(data.body.tracks.items);
+            res.json(data)
+        })
+        .catch(err => console.log('The error while searching artists occurred: ', err))
 }
