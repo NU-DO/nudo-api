1d98ac6e3cf986c5da5e7c332bbe29c808566655
fermarinsanchez <fermarinsanchez@yahoo.es>
2021-03-22 23:26:26 +0100

    fix: updating cors middleware

--- config/cors.config.js
+++ config/cors.config.js
@@ -1,7 +1,7 @@
 const cors = require('cors')
 
 const corsMiddleware = cors({
-  origin: process.env.CORS_ORIGIN || 'https://ironnudo.herokuapp.com', // 'http://localhost:3000'
+  origin: 'https://ironnudo.herokuapp.com', // 'http://localhost:3000'
   allowedHeaders: ['Content-Type'],
   credentials: true
 })
