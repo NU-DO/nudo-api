11a005791c254360bb3b898e9ff1d875100ab533
fermarinsanchez <fermarinsanchez@yahoo.es>
2021-03-22 23:12:38 +0100

    fix: updating cors middleware

--- app.js
+++ app.js
@@ -16,7 +16,8 @@ if (app.get('env') === 'production') {
   app.set('trust proxy', 1)
 }
 
-//app.use(cors)
+app.use(cors)
+app.options('*', cors());
 app.use(logger('dev'))
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
--- config/cors.config.js
+++ config/cors.config.js
@@ -3,7 +3,8 @@ const cors = require('cors')
 const corsMiddleware = cors({
   origin: 'https://ironnudo.herokuapp.com' || process.env.CORS_ORIGIN , // 'http://localhost:3000'
   allowedHeaders: ['Content-Type'],
-  credentials: true
+  credentials: true,
+  
 })
 
 module.exports = corsMiddleware
