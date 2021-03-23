5b86c5c76febaf9a8f1176e9f54a186b45d39c35
fermarinsanchez <fermarinsanchez@yahoo.es>
2021-03-23 13:29:14 +0100

    fix: updating cors middleware

--- config/cors.config.js
+++ config/cors.config.js
@@ -1,9 +1,9 @@
 const cors = require('cors')
 
 const corsMiddleware = cors({
-  origin: process.env.CORS_ORIGIN || 'http://nudo.herokuapp.com',
+  origin: true, //process.env.CORS_ORIGIN || 'http://nudo.herokuapp.com',
   allowedHeaders: ['Content-Type'],
-  credentials: false
+  credentials: true
 })
 
 module.exports = corsMiddleware
